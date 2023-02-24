fafa673e1acb74c351014569b207c3d8307cbe82
khalil <khalil2cheddadi@gmail.com>
2023-02-24 22:22:23 +0100

    added tailwind to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-function App() {
+import "./index.css"
 
-  return <h1>Detect++ is deployed!!</h1>;
+function App() {
+  return <h1 className="text-3xl text-center font-bold blur-sm hover:blur-0 text-gray-900 duration-700">Hello world!</h1>;
 }
 
-export default App
+export default App;
